86b57fe2a1ab6311c5b6dbe1ba8a4810afa253c5
oguilhermero <rcunha.guilherme@gmail.com>
2024-11-07 12:49:55 -0300

    add three animals to the database

--- resources/database.js
+++ resources/database.js
@@ -1,9 +1,30 @@
-const animals {
+const animals = [
     {
-        name: ;
-        color: ;
-        hexColor: ;
-        sound: ;
-        funFact: ;
-    }
-}
+        name: "frog",
+        color: "green",
+        hexColor: "#89B07F",
+        sound: "ribbit",
+        funFact: "Many frogs can jump 20 times their own height"
+    },
+    {
+        name: "ant",
+        color: "black",
+        hexColor: "#000000",
+        sound: "sublte squeak",
+        funFact: "Ants have two stomachs"
+    },
+    {
+        name: "wolf spider",
+        color: "tan with dark brown stripes",
+        hexColor: "#d2b48c",
+        sound: "subtle purr",
+        funFact: "Wolf spiders do not spin webs"
+    },
+    {
+        name: "caterpillar",
+        color: "light green",
+        hexColor: "#7fb529",
+        sound: "whistle",
+        funFact: "Caterpillars have 12 tiny eyelets that are known as stemmata"
+    },
+]
